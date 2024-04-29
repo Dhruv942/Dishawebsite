50ea2d28d0b1462ae2d1dd7e8878d594f084c83c
Dhruv Patel <dhruvpatel242231@gmail.com>
2024-04-29 13:14:24 +0530

    hi

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Bg from "../assest/bg.jpg";
 import circle from "../assest/circle.png";
-
+import earth from "../assest/earth.json";
+import Lottie from "lottie-react";
 const Home = () => {
   return (
     <div className="flex flex-col items-center">
@@ -12,19 +13,79 @@ const Home = () => {
         className="w-full sticky"
       />
 
-      <div className="max-w-6xl mx-auto bg-white rounded-3xl shadow-md overflow-hidden md:max-w-6xl lg:max-w-7xl mt-24 p-8">
+      {/*new*/}
+      <div className="mt-7 flex flex-col md:flex-row">
+        <div className="mr-0 md:mr-10 mb-7 md:mb-0">
+          <div
+            style={{
+              width: "100%", // Adjusted to full width on small screens
+              maxWidth: "490px", // Added to limit width on larger screens
+              height: "200px",
+              backgroundColor: "#fafafa",
+            }}
+            className="p-4"
+          >
+            <div className="">
+              <div className="text-3xl font-bold mb-3 md:mb-0 md:mr-4">
+                Our Vision
+              </div>
+              <div className="text-xs md:mt-3 from-neutral-400 ">
+                <h5>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Minima eveniet repudiandae rem delectus dolores illo nam in
+                  non repellat obcaecati quae tempora cum, adipisci ducimus,
+                  nemo quis aliquid. Cumque, nobis.
+                </h5>
+              </div>
+              <div className="mt-16 border-4 border-green-400"></div>
+            </div>
+          </div>
+        </div>
+        {/*new*/}
+        {/*new2*/}
+        <div className="mr-0 md:mr-10">
+          <div
+            style={{
+              width: "100%", // Adjusted to full width on small screens
+              maxWidth: "490px", // Added to limit width on larger screens
+              height: "200px",
+              backgroundColor: "#fafafa",
+            }}
+            className="p-4"
+          >
+            <div className="">
+              <div className="text-3xl font-bold mb-3 md:mb-0 md:mr-4">
+                Our Misisson
+              </div>
+              <div className="text-xs md:mt-3 from-neutral-400">
+                <h5>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Minima eveniet repudiandae rem delectus dolores illo nam in
+                  non repellat obcaecati quae tempora cum, adipisci ducimus,
+                  nemo quis aliquid. Cumque, nobis.
+                </h5>
+              </div>
+              <div className="mt-16 border-4 border-amber-400"></div>
+            </div>
+          </div>
+        </div>
+        {/*new2*/}
+      </div>
+      {/*1 */}
+      <div className="max-w-6xl mx-auto bg-red-100 rounded-3xl shadow-md overflow-hidden md:max-w-6xl lg:max-w-7xl mt-24 p-8">
         <div className="md:flex">
           <div className="md:shrink-0">
-            <img
+            <Lottie
+              animationData={earth}
+              loop={true}
               className="h-64 w-full object-cover md:h-auto md:w-64"
-              src={circle}
-              alt="Modern building architecture"
             />
+            ;
           </div>
 
           <div className="md:ml-8 mt-4 md:mt-0">
             <div className="uppercase tracking-wide text-4xl text-black font-extrabold mt-2">
-              DishaFoundation
+              Disha Foundation
             </div>
 
             <p className=" text-black mt-8">
@@ -39,58 +100,7 @@ const Home = () => {
           </div>
         </div>
       </div>
-
-      <div className="flex flex-wrap justify-center mt-12">
-        {" "}
-        {/* Added flex-wrap and justify-center */}
-        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ">
-          {/* Added margin */}
-          <a href="#">
-            <img className="rounded-t-lg" src={Bg} alt="" />
-          </a>
-          <div className="p-5 text-xl ">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Our Vision
-            </h5>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-          </div>
-        </div>
-        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 ">
-          {" "}
-          {/* Added margin */}
-          <a href="#">
-            <img className="rounded-t-lg" src={Bg} alt="" />
-          </a>
-          <div className="p-5">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Our Mission
-            </h5>
-
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-          </div>
-        </div>
-        <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <a href="#">
-            <img class="rounded-t-lg" src={Bg} alt="" />
-          </a>
-          <div class="p-5">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Our Goal
-            </h5>
-
-            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-              Here are the biggest enterprise technology acquisitions of 2021 so
-              far, in reverse chronological order.
-            </p>
-          </div>
-        </div>
-      </div>
+      <div>sca</div>
     </div>
   );
 };
